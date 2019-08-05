4c3d8325a8145c6b247a684f376bd11509de828b
Tracey Wright <traceywright@Traceys-MBP.gateway>
2019-08-05 11:33:56 +1000

    minor styling

--- disease-classifier/client/src/components/contact/ContactComponent.tsx
+++ disease-classifier/client/src/components/contact/ContactComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const ContactComponent = () => (
-  <div style={{textAlign: "center", color: "rgba(255,255,255,.5)", marginTop: "100px"}}>
+  <div style={{textAlign: "center", color: "rgba(255,255,255,.5)", paddingTop: "100px"}}>
     <h4>Tracey Wright</h4>
     <h4>tracelouisewright@hotmail.com</h4>
     <h4>0421 949 578</h4>
