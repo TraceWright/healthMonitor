38454771c5e11fe02b6548e24ed19084b6127dce
Tracey Wright <traceywright@Traceys-MBP.gateway>
2019-08-05 11:23:00 +1000

    add contact details

--- disease-classifier/client/src/App.tsx
+++ disease-classifier/client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Hypertension from './components/bloodPressure/BloodPressureComponent';
 import EgfrComponent from './components/egfr/EgfrComponent';
 import HistoryComponent from './components/history/HistoryComponent';
+import ContactComponent from './components/contact/ContactComponent';
 import MenuContainer from "./components/menu/menu";
 
 const App: React.FC = () => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Route path="/bloodpressure/" component={Hypertension} />
         <Route path="/egfr/" component={EgfrComponent} />
         <Route path="/history/" component={HistoryComponent} />
+        <Route path="/contact/" component={ContactComponent} />
     </BrowserRouter>
   );
 }
---
+++ disease-classifier/client/src/components/contact/ContactComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ContactComponent = () => (
+  <div style={{textAlign: "center", color: "rgba(255,255,255,.5)", marginTop: "100px"}}>
+    <h4>Tracey Wright</h4>
+    <h4>tracelouisewright@hotmail.com</h4>
+    <h4>0421 949 578</h4>
+  </div>
+)
+
+export default ContactComponent;
